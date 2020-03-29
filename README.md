c1b033bd0f496161151aab05ad9f6692a57ad679
Fabian Müller <fabian.mueller@statworx.com>
2020-03-29 16:20:27 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Forecasting-XAI
+# Daimler Trucks (FTC/BA) XAI Pitch
+
+## Ressources
+- https://pbiecek.github.io/PM_VEE/
+- https://christophm.github.io/interpretable-ml-book/intro.html
--- setup.R
+++ setup.R
@@ -18,13 +18,14 @@ library(formula.tools)
 library(xgboost)
 library(DT)
 library(gridExtra)
+library(kableExtra)
 
 col_pal <- magma(5)
 
 source("train_xgb.R")
 source("create_Z_varest.R")
 
-
+# Daimler colors
 dci_palette <- function() {
   
   m_blue_grey <- rgb(52, 64, 77, maxColorValue = 255)
@@ -43,3 +44,93 @@ dci_palette <- function() {
            m_red,
            m_black))
 }
+
+# custom function to plot lime objects
+plot_lime <- function(lime_object, model_type){
+  title_string <- plot(lime_object)$labels$title
+  
+  lime_object$results %>%
+    ggplot() +
+    geom_bar(aes(x = reorder(feature.value, effect),
+                 y = effect),
+             stat = "identity",
+             fill = "#5E7E90",
+             alpha = 0.95) +
+    coord_flip() +
+    theme_minimal() +
+    labs(x = "",
+         y = "Feature Attribution",
+         title = title_string,
+         subtitle = model_type)
+}
+
+# custom function to plot breakdwon plots
+
+plot_waterfall <- function(explain_object, title_string){
+  
+  df_explained <- as.data.frame(explain_object) 
+  
+  intercept <- df_explained %>%
+    dplyr::filter(variable == "intercept") %>%
+    dplyr::pull(contribution)
+  
+  prediction <- df_explained %>%
+    dplyr::filter(variable == "prediction") %>%
+    dplyr::pull(contribution)
+  
+  df_explained <- df_explained %>%
+    dplyr::select(-label) %>%
+    dplyr::select(variable, contribution, end = cummulative, value = variable_value, sign, position) %>% 
+    dplyr::mutate(start = dplyr::lag(end),
+                  sign_new = case_when(sign == "1" ~ "+",
+                                       sign == "0" ~ "",
+                                       sign == "-1" ~ "-",
+                                       TRUE ~ "+"),
+                  value = as.numeric(as.character(value)),
+                  label = paste(gsub(pattern = " =.*", replacement = "", x = variable),
+                                "=", 
+                                round(value)),
+                  label = ifelse(variable == "prediction", "prediction", label),
+                  label = ifelse(variable == "intercept", "intercept", label)) %>%
+    dplyr::mutate(start = ifelse(variable == "intercept", intercept, start)) %>% 
+    dplyr::arrange(position)
+  
+  df_explained %>% 
+    ggplot(., aes(x = reorder(label, position)),
+           alpha = 0.9) + 
+    geom_segment(aes(xend = label,
+                     y = ifelse(label == "intercept", end, start),
+                     yend = end,
+                     colour = sign_new),
+                 size = 7) +
+    geom_segment(data = dplyr::filter(df_explained, variable == "prediction"),
+                 aes(xend = label, 
+                     y = end, 
+                     yend = intercept), 
+                 size = 7,
+                 color = "#34404D") +
+    geom_text(aes(x = label, 
+                  y = ifelse(sign_new == "-", start, end), 
+                  label = round(contribution, 3),
+                  fontface = ifelse(variable == "prediction", 2, 1)), 
+              size = 18,
+              nudge_y = mean(abs(df_explained$contribution))/8) +
+    geom_hline(aes(yintercept = intercept),
+               lty = "dashed",
+               alpha = 0.5) +
+    scale_color_manual("",
+                       values = c("-" = "firebrick4",
+                                  "+" = "#50BC8A",
+                                  "0" = "black"),
+                       labels = c("-" = "Decreasing Prediction",
+                                  "+" = "Increasing Prediction",
+                                  "0" = "Neutral")) +
+    coord_flip() +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    guides(color = guide_legend(override.aes = list(size = 5))) +
+    labs(x = "", y = "", subtitle = title_string)
+}
+
+
+
